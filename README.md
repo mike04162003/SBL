28978084284f1a9a4c3d8a2d5a92b6b110a64b93
Mike.Fan <mike.fan@advantech.com.tw>
2025-12-03 00:13:45 +0800

    Update commit

--- README.md
+++ README.md
@@ -14,7 +14,6 @@ SOM-6884|688400S|0x1D|RaptorlakeBoardPkg|LLDR;UEFI|Bring up to OS
 SOM-C350|C35000S|0x1C|AlderlakeBoardPkg|LLDR;UEFI|Bring up to uefi shell
 
 
-
 FSP Project Name | Directory Name | FSP Specification Version
 :--------------- | :------------- | :------------------------
 Intel Core Ultra Processors for IoT Edge (Formerly known as Meteor Lake-UH and Meteor Lake-PS). | MeteorlakeFspBinPkg | v2.3
@@ -26,7 +25,7 @@ SOM-A350|A35000S|0x01|MeteorlakeBoardPkg|LLDR;UEFI|Bring up sbl shell
 
 FSP Project Name | Directory Name | FSP Specification Version
 :--------------- | :------------- | :------------------------
-Intel XeonR D-1700 and D-2700 Processor Family (formerly Ice Lake-D LCC and HCC) | IdavilleFspBinPkg | v2.1
+Intel Xeon D-1700 and D-2700 Processor Family (formerly Ice Lake-D LCC and HCC) | IdavilleFspBinPkg | v2.1
 
 Porject Name | Porject ID | Platform ID | Directory Name | Payload Type | Status
 :----------- |:----------- |:-------------- | :---------- | :----------  | :----------
@@ -69,6 +68,12 @@ Porject Name | Porject ID | Platform ID | Directory Name | Payload Type | Status
 SOM-7569|756900S|0x13|ApollolakeBoardPkg|LLDR;UEFI|Bring up to OS
 
 
+Slim Bootloader support uefipayload
+===================================
+
+      repo : https://github.com/Advgcipc/edk2.git
+    commit : 89709f519eecfc2d79d9492c3f796266cd6b02e1
+
 Build Environment
 -----------------
 
@@ -79,11 +84,8 @@ Build Environment
 
   Usage:
     
-    python Sbl.py -p $(Porject ID)
+    python Sbl.py -p 'ahclsbl','edk','advgcipc','sbl' -c $(Porject ID)
     
-    or
-    
-    slim -p $(Porject ID)
 
 License
 -------
--- Sbl.py
+++ Sbl.py
@@ -1,10 +1,9 @@
 ## @ Sbl.py
 #
-# Copyright (c) 2018 - 2026, Advantech Co Ltd. All rights reserved.<BR>
-# SPDX-License-Identifier: BSD-2-Clause-Patent
-#
 #
 # Copyright (c) 2018 - 2021, Intel Corporation. All rights reserved.<BR>
+# Copyright (c) 2025 - 2026, Advantech Co Ltd. All rights reserved.<BR>
+#
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 #
 ##
@@ -31,7 +30,7 @@ def Print (msg):
 
 def GitCloneRepo (clone_dir, clone_repo, clone_commit):
 
-    if clone_repo == '' :
+    if clone_repo == '' and clone_commit == '':
         Fatal ('Failed to find repo and commit information!')
 
     Print (' clone_repo: %s\n clone_commit: %s' % (clone_repo, clone_commit))
@@ -50,27 +49,43 @@ def GitCloneRepo (clone_dir, clone_repo, clone_commit):
         if ret:
             Fatal ('Failed to clone repo to directory %s !' % clone_dir)
         Print ('GitCloneRepo - clone Done\n')
-        Print ('Attempting to check out specified version ... %s' % clone_commit)
-        cmd = 'git checkout %s' % clone_commit
-        ret = subprocess.call(cmd.split(' '), cwd=clone_dir)
-        if ret == 0:
-            Print ('GitCloneRepo - checkout Done\n')
-            return clone_dir
-        else:
-            Print ('GitCloneRepo - checkout: Failed to checkout  commit: %s' % clone_commit)
+
+        if not clone_commit == '':
+            Print ('Attempting to check out specified version ... %s' % clone_commit)
+            cmd = 'git checkout %s' % clone_commit
+            ret = subprocess.call(cmd.split(' '), cwd=clone_dir)
+            if ret == 0:
+                Print ('GitCloneRepo - checkout Done\n')
+                return clone_dir
+            else:
+                Print ('GitCloneRepo - checkout: Failed to checkout  commit: %s' % clone_commit)
     else:
         # If the repository already exists, then try to check out the correct
         # revision without going to the network
-        Print ('Attempting to check out specified version ... %s' % clone_commit)
-        cmd = 'git checkout %s' % clone_commit
-        ret = subprocess.call(cmd.split(' '), cwd=clone_dir)
-        if ret == 0:
-            Print ('GitCloneRepo - checkout Done\n')
-            return clone_dir
-        else:
-            Print ('GitCloneRepo - checkout: Failed to checkout  commit: %s' % clone_commit)
+        if not clone_commit == '':
+            Print ('Attempting to check out specified version ... %s' % clone_commit)
+            cmd = 'git checkout %s' % clone_commit
+            ret = subprocess.call(cmd.split(' '), cwd=clone_dir)
+            if ret == 0:
+                Print ('GitCloneRepo - checkout Done\n')
+                return clone_dir
+            else:
+                Print ('GitCloneRepo - checkout: Failed to checkout  commit: %s' % clone_commit)
 
-def get_platform_type (arg_platform):
+
+def get_repo_git (arg_repo):
+    platform_type ={
+        'ahclsbl'    :   'https://github.com/mike04162003/AHCLSbl.git',
+        'edk'        :   'https://github.com/mike04162003/EDK.git',
+        'advgcipc'   :   'https://github.com/Advgcipc/slimbootloader.git',
+        'sbl'        :   'https://github.com/slimbootloader/slimbootloader.git'
+    }
+
+    ModuleType = platform_type.get(arg_repo, "")
+
+    return ModuleType
+
+def get_advgcipc_commit (arg_platform):
     platform_type ={
         '688400S'    :   'SOM-6884',
         '6884A2S'    :   'SOM-6884A2',
@@ -84,15 +99,64 @@ def get_platform_type (arg_platform):
         '756900S'    :   'ADV'
     }
 
+    ModuleType = platform_type.get(arg_platform, "")
+
+    return ModuleType
+
+def get_sbl_commit (arg_platform):
+    platform_type ={
+        '688400S'    :   '7a5041ee',
+        '6884A2S'    :   '7a5041ee',
+        '758300S'    :   '7a5041ee',
+        '599300S'    :   '7a5041ee',
+        'D58000S'    :   '7a5041ee',
+        'C35000S'    :   '7a5041ee',
+        'DS20200'    :   '7a5041ee',
+        'A28700A'    :   '7a5041ee',
+        'A58600A'    :   '7a5041ee',
+        '756900S'    :   '7a5041ee'
+    }
 
     ModuleType = platform_type.get(arg_platform, "")
 
-    sku_dict = platform_type
-    sku = arg_platform
-    for sku in platform_type:
-        platform_type =  sku
-        Print ("No sku is given, set to default sku value %s" % sku)
+    return ModuleType
 
+def get_ahclsbl_commit (arg_platform):
+    platform_type ={
+        '688400S'    :   'SOM-6884',
+        '6884A2S'    :   'SOM-6884A2',
+        '758300S'    :   'SOM-7583',
+        '599300S'    :   'SOM-5993',
+        'D58000S'    :   'SOM-D580',
+        'C35000S'    :   'SOM-C350',
+        'DS20200'    :   'DS-202',
+        'A28700A'    :   'AIMB-287',
+        'A58600A'    :   'AIMB-586',
+        '756900S'    :   'ADV'
+    }
+
+    ModuleType = platform_type.get(arg_platform, "")
+    ModuleType = 'main'
+
+    return ModuleType
+
+def get_edk_commit (arg_platform):
+    platform_type ={
+        '688400S'    :   'SOM-6884',
+        '6884A2S'    :   'SOM-6884A2',
+        '758300S'    :   'SOM-7583',
+        '599300S'    :   'SOM-5993',
+        'D58000S'    :   'SOM-D580',
+        'C35000S'    :   'SOM-C350',
+        'DS20200'    :   'DS-202',
+        'A28700A'    :   'AIMB-287',
+        'A58600A'    :   'AIMB-586',
+        '756900S'    :   'ADV'
+    }
+
+    ModuleType = platform_type.get(arg_platform, "")
+
+    ModuleType = 'main'
     return ModuleType
 
 def run_cmd(cmd_list):
@@ -104,23 +168,32 @@ def run_cmd(cmd_list):
 
 def Main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('-g',   dest='--getsbl', default = '', help='get slim bootloader and platform source')
-    ap.add_argument('-p',   dest='platform', default = '', choices=['688400S','6884A2S','758300S','599300S','D58000S','C35000S','DS20200','A28700A','A58600A','756900S'], help='specify Advantech project name')
+    ap.add_argument('-g', dest='repo',    default = '', choices=['ahclsbl','edk','advgcipc','sbl'], help='specify git project name')
+    ap.add_argument('-c', dest='commit',  default = '', choices=['main','688400S','6884A2S','758300S','599300S','D58000S','C35000S','DS20200','A28700A','A58600A','756900S'], help='specify commit id')
     args = ap.parse_args()
 
     repo_dir = os.path.abspath (os.path.join(os.getcwd(), '$AUTO'))
-#    repo = 'https://github.com/Advgcipc/AdvantechSBL.git'
-#    repo = 'https://github.com/Advgcipc/slimbootloader.git'
-    commit = get_platform_type (args.platform)
+    repo = get_repo_git (args.repo)
+    commit = ''
+
+    if args.repo == 'advgcipc':
+        commit = get_advgcipc_commit (args.commit)
+    if args.repo == 'ahclsbl':
+        commit = get_ahclsbl_commit (args.commit)
+    if args.repo == 'edk':
+        commit = get_edk_commit (args.commit)     
+    if args.repo == 'sbl':
+        commit = get_sbl_commit (args.commit)
 
     Print ("repo_dir :  %s\n"  % repo_dir)
-#    Print ("    repo :  %s\n"  % repo)
+    Print ("    repo :  %s\n"  % repo)
     Print ("  commit :  %s\n"  % commit)
 
-#    GitCloneRepo (repo_dir, repo, commit)
-    script_dir = os.path.abspath (os.path.join(os.getcwd(), 'AHCLSbl','Tools'))
-    Print ("script_dir :  %s\n"  % script_dir)
-    run_cmd([sys.executable, os.path.join(script_dir, 'AHCLSbl.py')])
+    GitCloneRepo (repo_dir, repo, commit)
+#    script_dir = os.path.abspath (os.path.join(os.getcwd(), 'AHCLSbl','Tools'))
+#    Print ("script_dir :  %s\n"  % script_dir)
+#    run_cmd([sys.executable, os.path.join(script_dir, 'AHCLSbl.py')])
+
  
     return 0
 
